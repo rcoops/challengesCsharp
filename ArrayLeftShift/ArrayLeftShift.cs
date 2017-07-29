78603238b0ec46f50f227d06162c2cf9e4684147
Rick Cooper <rcooper@sis.tv>
2017-07-29 07:13:13 +0100

    first 2 challenges

---
+++ ArrayLeftShift/ArrayLeftShift.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace ArrayLeftShift
+{
+    internal class ArrayLeftShift
+    {
+        public static void Main(string[] args)
+        {
+            var arguments = Console.ReadLine()?.Split(' ');
+            var input = Console.ReadLine()?.Split(' ').Select(int.Parse).ToArray();
+            if (input == null || arguments == null || arguments.Length != 2)
+            {
+                PrintUsageInstructionsAndExit();
+            }
+            else
+            {
+                var arraySize = int.Parse(arguments[0]);
+                var leftShift = int.Parse(arguments[1]);
+                if(arraySize != input.Length)
+                {
+                    PrintUsageInstructionsAndExit();
+                }
+                int[] output = LeftShift(input, leftShift);
+                Console.WriteLine(string.Join(" ", output));
+            }
+        }
+
+        private static int[] LeftShift(int[] input, int leftShift)
+        {
+            int[] output = new int[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                var adjustment = i - leftShift % input.Length;
+                var newIndex = adjustment < 0 ? input.Length + adjustment : adjustment;
+                output[newIndex] = input[i];
+            }
+            return output;
+        }
+
+        private static void PrintUsageInstructionsAndExit()
+        {
+            StringBuilder sb = new StringBuilder("Usage: <Array length> <Units to shift>\n");
+            sb.Append("<Array elements>\ne.g:\n5 2\n1 2 3 4 5\n");
+            sb.Append("NOTE: Number of elements must match length");
+            Console.WriteLine(sb.ToString());
+            Environment.Exit(1);
+        }
+        
+    }
+    
+}
---
+++ BinaryNumbers/BinaryNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace BinaryNumbers
+{
+    internal class BinaryNumbers
+    {
+        
+        static void Main(String[] args)
+        {
+            int n = -1;
+            try
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                PrintUsageInstructionsAndExit();
+            }
+            StringBuilder sb = new StringBuilder();
+            while(n > 0) {
+                sb.Insert(0, (byte) n % 2);
+                n /= 2;
+            }
+            String binaryNumber = sb.ToString();
+            Console.WriteLine(binaryNumber);
+            var splitByOnes = binaryNumber.Split('0');
+            Console.WriteLine(splitByOnes.Max(consecutiveOnes => consecutiveOnes.Length));
+        }
+
+        private static void PrintUsageInstructionsAndExit()
+        {
+            StringBuilder sb = new StringBuilder("Usage: <Number To Convert to Binary>\n");
+            sb.Append("e.g:\n999999\n");
+            sb.Append("NOTE: Number of must be within 1 & 1000000");
+            Console.WriteLine(sb.ToString());
+            Environment.Exit(1);
+        }
+        
+    }
+    
+}
